3daa4b6bbf1598bc42228880d3bac1db19b21343
Manuel Bogino <manuel.bogino@dataart.com>
2022-10-18 14:29:31 -0300

    Setup jest and react testing library

---
+++ __tests__/components/features/ProductCard.test.tsx
@@ -0,0 +1,6 @@
+import ProductCard from 'components/features/ProductCard';
+import { render } from 'lib/test-utils';
+
+describe(ProductCard, () => {
+  it('should render a link', () => {});
+});
---
+++ __tests__/components/ui/Button.test.tsx
@@ -0,0 +1,12 @@
+import { Button } from 'components/ui';
+import { render, screen } from 'lib/test-utils';
+
+describe(Button, () => {
+  it('should render a button', () => {
+    render(<Button text="Button" />);
+
+    const button = screen.getByRole('button', { name: /Button/ });
+
+    expect(button).toBeVisible();
+  });
+});
